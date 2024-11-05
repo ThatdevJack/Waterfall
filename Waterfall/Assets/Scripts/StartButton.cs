94667b2974cf6413da0fef776431e05ea0970bd5
ThatdevJack <93835941+ThatdevJack@users.noreply.github.com>
2024-11-05 12:36:45 -0500

    Unity project

---
+++ Waterfall/Assets/Scripts/StartButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+
+    public string sceneName;
+
+    public void OnMouseClick()
+    {
+
+
+
+
+
+        SceneManager.LoadScene(sceneName);
+
+
+
+    }
+
+    public void AdventureMouseClick()
+    {
+        if(PlayerPrefs.GetInt("playedAdventure") == 1)
+        {
+            string savedScene = PlayerPrefs.GetString("LoadScene");
+            SceneManager.LoadScene(savedScene);
+        }
+        else
+        {
+            OnMouseClick();
+        }
+    }
+}
